25fcc59919ed62685c6a5fb2556cb3ffac66ce7b
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-10-26 14:53:43 -0700

    updated bank account

--- bank_account.py
+++ bank_account.py
@@ -1,3 +1,9 @@
 class BankAccount:
     def __init__(self, name, account_number, balance):
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
         pass
+
+    def deposit(self, amount):
+        self.balance += amount
