e340fa574d38976989d7443a54110653e8152fdd
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-10-27 15:33:11 -0700

    updated ui

--- bank_account.py
+++ bank_account.py
@@ -39,7 +39,7 @@ class BankAccount:
         self.balance += interest
 
     def print_statement(self):
-        print(f"{self.name}\nAccount No.: {self.account_number}\nRouting No.: {self.route_number}\nBalance: ${self.balance:.2f}\nAccount Type: {self.account_type.capitalize()}")
+        print(f"{self.name}\nAccount No.: {self.display_account_number}\nRouting No.: {self.route_number}\nBalance: ${self.balance:.2f}\nAccount Type: {self.account_type.capitalize()}")
 
 class Bank:
     def __init__(self):
